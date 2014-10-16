e2e6eb50c9851c40a9be65d94da4378e800e1511
J. Sheth <jsheth7@gmail.com>
2014-10-15 22:49:39 -0400

    "Play with storyboard" - not quite working yet

---
+++ unit3/play with storyboard/play with storyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  play with storyboard
+//
+//  Created by Jayesh Sheth on 10/15/14.
+//  Copyright (c) 2014 Jayesh Sheth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
